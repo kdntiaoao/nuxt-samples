9c22a0fb244e6ddeb64c8faf6cee042998a05753
iadokonat <iadokonat0106@gmail.com>
2023-04-03 13:26:08 +0900

    カメラ起動ボタンを常時表示する

--- pages/ocr/index.vue
+++ pages/ocr/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="wrap">
-    <button v-if="!isActiveCamera" type="button" @click="startCamera">
-      カメラ起動
-    </button>
-    <div v-else>
+    <button v-if="true" type="button" @click="startCamera">カメラ起動</button>
+    <div>
       <video width="300" height="200" class="video" ref="video"></video>
       <div class="img-box">
         <canvas class="canvas" ref="canvasFull"></canvas>
@@ -28,6 +26,60 @@ const canvasFull = ref<HTMLCanvasElement | null>(null);
 const canvasView = ref<HTMLCanvasElement | null>(null);
 const result = ref<HTMLParagraphElement | null>(null);
 
+const videoToCanvas = () => {
+  if (!video.value || !canvasFull.value) return;
+
+  canvasFull.value.width = VIEW_WIDTH + 50;
+  canvasFull.value.height =
+    (canvasFull.value.width * video.value.videoHeight) / video.value.videoWidth;
+
+  const ctx = canvasFull.value.getContext('2d');
+
+  if (!ctx) return;
+
+  ctx.drawImage(
+    video.value,
+    0,
+    0,
+    video.value.videoWidth,
+    video.value.videoHeight,
+    0,
+    0,
+    canvasFull.value.width,
+    canvasFull.value.height
+  );
+
+  const viewRange = {
+    x: (canvasFull.value.width - VIEW_WIDTH) / 2,
+    y: (canvasFull.value.height - VIEW_HEIGHT) / 2,
+    w: VIEW_WIDTH,
+    h: VIEW_HEIGHT,
+  };
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 2;
+  ctx.rect(viewRange.x, viewRange.y, viewRange.w, viewRange.h);
+  ctx.stroke();
+
+  canvasView.value = document.createElement('canvas');
+  canvasView.value.width = VIEW_WIDTH;
+  canvasView.value.height = VIEW_HEIGHT;
+  canvasView.value
+    .getContext('2d')
+    ?.drawImage(
+      canvasFull.value,
+      viewRange.x,
+      viewRange.y,
+      viewRange.w,
+      viewRange.h,
+      0,
+      0,
+      viewRange.w,
+      viewRange.h
+    );
+};
+
 const startCamera = () => {
   if (process.client) {
     navigator.mediaDevices
@@ -43,58 +95,7 @@ const startCamera = () => {
         video.value.play();
 
         setInterval(() => {
-          if (!video.value || !canvasFull.value) return;
-
-          canvasFull.value.width = VIEW_WIDTH + 50;
-          canvasFull.value.height =
-            (canvasFull.value.width * video.value.videoHeight) /
-            video.value.videoWidth;
-
-          const ctx = canvasFull.value.getContext('2d');
-
-          if (!ctx) return;
-
-          ctx.drawImage(
-            video.value,
-            0,
-            0,
-            video.value.videoWidth,
-            video.value.videoHeight,
-            0,
-            0,
-            canvasFull.value.width,
-            canvasFull.value.height
-          );
-
-          const viewRange = {
-            x: (canvasFull.value.width - VIEW_WIDTH) / 2,
-            y: (canvasFull.value.height - VIEW_HEIGHT) / 2,
-            w: VIEW_WIDTH,
-            h: VIEW_HEIGHT,
-          };
-
-          ctx.beginPath();
-          ctx.strokeStyle = 'red';
-          ctx.lineWidth = 2;
-          ctx.rect(viewRange.x, viewRange.y, viewRange.w, viewRange.h);
-          ctx.stroke();
-
-          canvasView.value = document.createElement('canvas');
-          canvasView.value.width = VIEW_WIDTH;
-          canvasView.value.height = VIEW_HEIGHT;
-          canvasView.value
-            .getContext('2d')
-            ?.drawImage(
-              canvasFull.value,
-              viewRange.x,
-              viewRange.y,
-              viewRange.w,
-              viewRange.h,
-              0,
-              0,
-              viewRange.w,
-              viewRange.h
-            );
+          videoToCanvas();
         }, 200);
       })
       .catch((e) => {
