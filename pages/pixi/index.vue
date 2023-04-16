23df546fa26e06d3587a25964556477bb1c68ea1
iadokonat <iadokonat0106@gmail.com>
2023-04-17 05:50:29 +0900

    太陽を押すと惑星を増やせるようにした

--- pages/pixi/index.vue
+++ pages/pixi/index.vue
@@ -7,7 +7,19 @@ import * as PIXI from 'pixi.js'
 import { Sun } from '@/sprites/Sun'
 import { Planet } from '@/sprites/Planet'
 
+let planetCount = 0
+
 const canvasRef = ref<HTMLCanvasElement | null>(null)
+
+const addPlanet = (app: PIXI.Application) => {
+  planetCount++
+  const planet = new Planet(app.screen.width / 2, app.screen.height / 2, 100 + planetCount * 50)
+  app.stage.addChild(planet)
+
+  app.ticker.add(() => {
+    planet.revolve()
+  })
+}
 
 onMounted(() => {
   if (!canvasRef.value) return
@@ -21,16 +33,11 @@ onMounted(() => {
   })
   const sun = new Sun(app.screen.width / 2, app.screen.height / 2)
   app.stage.addChild(sun)
-  const planet1 = new Planet(app.screen.width / 2, app.screen.height / 2, 100)
-  app.stage.addChild(planet1)
-  const planet2 = new Planet(app.screen.width / 2, app.screen.height / 2, 150)
-  app.stage.addChild(planet2)
 
-  app.ticker.add((delta) => {
-    planet1.revolve()
+  sun.on('pointertap', (ev: PIXI.FederatedPointerEvent) => {
+    addPlanet(app)
   })
-  app.ticker.add((delta) => {
-    planet2.revolve()
-  })
+
+  addPlanet(app)
 })
 </script>
--- sprites/sun.ts
+++ sprites/sun.ts
@@ -1,11 +1,14 @@
 import * as PIXI from 'pixi.js'
 
+type AddPlanet = (app: PIXI.Application) => void
+
 export class Sun extends PIXI.Graphics {
   constructor(x: number, y: number) {
     super()
 
     this.x = x
     this.y = y
+    this.eventMode = 'static'
 
     this.beginFill(0xff3300).drawCircle(0, 0, 40).endFill()
   }
